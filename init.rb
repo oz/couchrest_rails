fdf209ac1b97af840258e4ce8b63c10af41501a6
Arnaud Berthomier <arnaud@cyprio.net>
2009-07-13 12:50:42 +0200

    Depend on our own changed couchrest

--- init.rb
+++ init.rb
@@ -2,6 +2,6 @@ require 'couch_rest_rails'
 require 'validatable'
 require 'spec/rails/matchers/couch_document_validations'
 
-config.gem 'couchrest'
+config.gem 'oz-couchrest', :lib => 'couchrest', :source => 'http://gems.github.com'
 config.gem 'validatable'
 config.gem 'json'
